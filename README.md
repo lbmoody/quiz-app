5c99bebe5cd775de9c0214a1fcde6fa35c0f2df1
lmoody <l.moody@continuumonline.com>
2019-12-13 21:32:23 -0700

    adding initial layout to application

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # quiz-app
-quiz application
+Front end quiz application that uses javascript to serve up questions and test the users knowledge on a subject.
---
+++ javascriptQuiz.js
@@ -0,0 +1,18 @@
+var jQuiz = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        options: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    }, 
+    {
+        question: "The condition in an if / else statement is enclosed within ____.",
+        options: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    }, 
+    {
+        question: "What color is at the top of a rainbow?",
+        options: ["red", "orange", "yellow", "green"], 
+        answer: "red"
+    }, 
+
+];
