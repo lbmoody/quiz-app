afdb186ca04d15724f8e935f10e30eac64aa25cb
lmoody <l.moody@continuumonline.com>
2019-12-16 22:35:28 -0700

    updating layout and html/css

--- assets/script.mjs
+++ assets/script.mjs
@@ -36,15 +36,15 @@ var DEBUG = true;
 // array for the quizzes
 var quizzes = [
       htmlQuiz
+      , javascriptQuiz
     , cssQuiz
-    , javascriptQuiz
 ];
 
 // array of quiz titles
 var quizTitles = [
       htmlQuizTitle
+      , javascriptQuizTitle
     , cssQuizTitle
-    , javascriptQuizTitle
 ];
 
 var landingSection = document.getElementById("landing");
@@ -62,6 +62,7 @@ var timeLeft = 0;
 var points = 0;
 var currentQuestion;
 var progressBarEl = document.createElement("div");
+var multiplierEl = document.createElement("h3");
 
 
 function quizzer() {
@@ -103,7 +104,7 @@ function orderOptions() {
         var index = currentQuestion.options.indexOf(option);
         optionButton.id = index;
         optionButton.setAttribute("type", "button");
-        optionButton.setAttribute("class", "btn btn-secondary m-2");
+        optionButton.setAttribute("class", "btn btn-secondary btn-lg m-2");
         optionButton.textContent = option;
         optionsEl.appendChild(optionButton);
     })
@@ -136,11 +137,14 @@ function quizTimer() {
 function createProgressBar() {
     var progressBarParent = document.createElement("div");
     progressBarParent.setAttribute("class", "progress");
+    multiplierEl.innerHTML = `Multiplier: <b>x${points}</b>`
+    multiplierEl.style.textAlign = "right";
+    progressBar.appendChild(multiplierEl);
     progressBar.appendChild(progressBarParent);
-    progressBarEl.setAttribute("class", "progress-bar bg-success");
+    progressBarEl.setAttribute("class", "progress-bar bg-success progress-bar-striped progress-bar-animated");
     progressBarEl.setAttribute("role", "progressbar");
     progressBarParent.appendChild(progressBarEl);
-    return progressBarEl;
+    
 }
 
 // Updates the progress bar to show how much time is left
@@ -148,17 +152,18 @@ function createProgressBar() {
 function updateProgressBar() {
     var progressPercent = (timeLeft) / (quizzes[quizID].length * 5) * 100;
     var percent = progressPercent.toFixed(2);
+    multiplierEl.innerHTML = `Multiplier: <b>x${points}</b>`
     progressBarEl.setAttribute("style", `width: ${percent}%`);
     progressBarEl.setAttribute("aria-valuenow", `${timeLeft}`);
     progressBarEl.setAttribute("aria-valuemin", "0");
     progressBarEl.setAttribute("aria-valuemax", `${quizzes[quizID].length * 5}`);
 
     if (percent < 66) {
-        progressBarEl.setAttribute("class", "progress-bar bg-warning");
+        progressBarEl.setAttribute("class", "progress-bar bg-warning progress-bar-striped progress-bar-animated");
     }
 
     if (percent < 33) {
-        progressBarEl.setAttribute("class", "progress-bar bg-danger");
+        progressBarEl.setAttribute("class", "progress-bar bg-danger progress-bar-striped progress-bar-animated");
     }
 
 }
@@ -170,7 +175,7 @@ quizzes.forEach(function(quiz) {
     quizButton.id = index;
     quizButton.textContent = `${quizTitles[index]} Quiz`;
     quizButton.setAttribute("type", "button");
-    quizButton.setAttribute("class", "btn btn-outline-secondary m-2 p-4");
+    quizButton.setAttribute("class", "btn btn-outline-secondary btn-lg m-2 p-4");
     quizSelectEl.appendChild(quizButton);
 })
 
